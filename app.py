b4f9346df424a83c89a24523318ada3a0a76b262
Jeff Shang <jingsun_us@yahoo.com>
2022-07-01 22:13:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import pickle
 from flask import Flask, request, render_template
 
-from detectron2.utils.logger import setup_logger
-setup_logger()
+#from detectron2.utils.logger import setup_logger
+#setup_logger()
 import cv2
 #from google.colab.patches import cv2_imshow
 from detectron2.engine import DefaultPredictor
