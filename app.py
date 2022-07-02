ebd197abf189a9080180ea3d4a7f10f8b0b901b8
Jeff Shang <jingsun_us@yahoo.com>
2022-07-02 15:45:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,32 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 
 app = Flask(__name__)
+model_root=""
+data_root=""
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+def GetKeypointsFromPredictor():
+    config_file_path = model_root+"config.yml"
 
+    weights_path = model_root+"model_final.pth"
+
+    image_path = data_root+"fencing.jpg"
+
+    model = config_file_path
+    im = cv2.imread(image_path)
+    cfg = get_cfg()
+    cfg.merge_from_file(config_file_path)
+
+    cfg.MODEL.WEIGHTS = weights_path
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.90
+    predictor = DefaultPredictor(cfg)
+    outputs = predictor(im)
+    keypoints = outputs["instances"].pred_keypoints
+    print("keypoints: ", keypoints)
+    return keypoints
 
 # prediction function
 def ValuePredictor(to_predict_list):
