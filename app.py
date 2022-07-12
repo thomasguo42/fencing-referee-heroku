72169952e5d59fd27fe24397eb8cad169ba4b360
Jeff Shang <jingsun_us@yahoo.com>
2022-07-11 21:08:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,38 @@ from filesplit.merge import Merge
 
 app = Flask(__name__)
 
+model_root=""
+data_root=""
+
 @app.route('/')
 def home():
     print("fencing home")
     return render_template('index.html')
+
+def GetKeypointsFromPredictor():
+    
+    
+    merge = Merge(inputdir="./model_files", outputdir="./", outputfilename="model_final_out.pth")
+    merge.merge()
+    
+    config_file_path = model_root+"config.yml"
+
+    weights_path = model_root+"model_final.pth"
+
+    image_path = data_root+"fencing.jpg"
+
+    model = config_file_path
+    im = cv2.imread(image_path)
+    cfg = get_cfg()
+    cfg.merge_from_file(config_file_path)
+
+    cfg.MODEL.WEIGHTS = weights_path
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.90
+    predictor = DefaultPredictor(cfg)
+    outputs = predictor(im)
+    keypoints = outputs["instances"].pred_keypoints
+    print("keypoints: ", keypoints)
+    return keypoints
 
 # prediction function
 def ValuePredictor(to_predict_list):
