af0072a95e8e98716c47432c6ab1ec9c885298c2
Jeff Shang <jingsun_us@yahoo.com>
2022-07-12 19:36:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def download_file(file_name, bucket):
     Function to download a given file from an S3 bucket
     """
     s3 = boto3.resource('s3')
-    output = f"downloads/{file_name}"
+    output = f"./{file_name}"
     s3.Bucket(bucket).download_file(file_name, output)
 
     return output
