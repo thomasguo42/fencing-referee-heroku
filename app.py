7001602d7948c767bfe78eaae557fda6bd74d636
Jeff Shang <jingsun_us@yahoo.com>
2022-07-15 10:33:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -94,6 +94,10 @@ def sign_s3():
   file_name = request.args.get('file-name')
   file_type = request.args.get('file-type')
 
+  print("start upload_file")
+  upload_file(file_name, S3_BUCKET)
+  print("end upload_file")
+    
   # Initialise the S3 client
   s3 = boto3.client('s3')
 
@@ -115,7 +119,33 @@ def sign_s3():
     'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
   })
     
-    
+import logging
+from botocore.exceptions import ClientError
+import os
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 # prediction function
  
 @app.route('/result', methods = ['POST'])
